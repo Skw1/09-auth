9c34a1fc4e421c6ca566db4007ee3201037410bf
Skwi <andrewdoluda@gmail.com>
2025-11-16 17:51:39 +0100

    fix

--- app/(private routes)/notes/[id]/NoteDetails.client.tsx
+++ app/(private routes)/notes/[id]/NoteDetails.client.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
-import { fetchNoteById } from "../../../lib/api";
-import type { Note } from "../../../../types/note";
+import { fetchNoteById } from "@/lib/api/clientApi";
+import type { Note } from "@/types/note";
 
 import css from "./NoteDetails.module.css";
 
